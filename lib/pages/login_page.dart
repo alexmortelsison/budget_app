7cd44a32d02cd32012f3f862b1f95acb25b6ac44
alexmortelsison <alexismortelsison@gmail.com>
2025-07-26 23:12:44 +0900

    updated loginpage

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,62 @@
+import 'package:budget_app/components/my_textfield.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.grey.shade100,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Lottie.asset("lib/assets/logo.json"),
+              Text(
+                "Budget App",
+                style: TextStyle(color: Colors.grey.shade700, fontSize: 24),
+              ),
+              SizedBox(height: 24),
+              MyTextfield(
+                hintText: "Email",
+                obscureText: false,
+                controller: emailController,
+              ),
+              SizedBox(height: 8),
+              MyTextfield(
+                hintText: "Password",
+                obscureText: true,
+                controller: passwordController,
+              ),
+              SizedBox(height: 4),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Not a member yet?",
+                  ),
+                  SizedBox(width: 4),
+                  Text(
+                    "Sign up now",
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
